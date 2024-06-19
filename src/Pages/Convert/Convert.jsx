629d1b326745003108330dbe646b3350f9d4c0b2
muh-osman <mr.muhammed.osman@gmail.com>
2024-06-19 23:20:38 +0300

    Fix

--- src/Pages/Convert/Convert.jsx
+++ src/Pages/Convert/Convert.jsx
@@ -1,11 +1,56 @@
-import style from './Convert.module.scss';
+import style from "./Convert.module.scss";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+// Loader
+import PropagateLoader from "react-spinners/PropagateLoader";
+// Counter
+// import SlotCounter from "react-slot-counter";
 
 export default function Convert() {
+  const [value, setValue] = useState(10);
+  let { number } = useParams();
 
+  const goToLink = () => {
+    window.location.href = `https://wa.me/${number}`;
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((prev) => {
+        if (prev === 0) {
+          clearInterval(interval);
+          window.location.href = `https://wa.me/${number}`;
+          return prev;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <div className={style.container}>
-      Convert
+      {/* <div className={style.counter}>
+        <SlotCounter
+          value={value}
+          sequentialAnimationMode
+          useMonospaceWidth
+          autoAnimationStart={false}
+        />
+      </div> */}
+
+      <div className={style.loader}>
+        <PropagateLoader color="#32d951" size={25} />
+      </div>
+
+      <div className={style.continue_btn_box}>
+        <button onClick={goToLink}>Continue to chat</button>
+      </div>
+
+      <div className={style.ad}>#Ad</div>
     </div>
-  )
+  );
 }
